026c7d37ec2d68b47f6339eb00f3616aecf1f35a
Pascal Böhler <pascal.boehler@hotmail.de>
2022-09-20 14:53:29 +0200

    feat: Added XCODE Files

---
+++ WeatherMe/WeatherMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMeApp.swift
+//  WeatherMe
+//
+//  Created by Pascal Boehler on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
